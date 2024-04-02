e6f20842ae0c563be018466a3dc5470697d752cb
uhumrenan <161661595+uhumrenan@users.noreply.github.com>
2024-04-01 21:20:43 -0300

    Aula 01/04/2024

--- src/ex_4/index.js
+++ src/ex_4/index.js
@@ -1,17 +1,46 @@
-import { View, TextInput, Text } from 'react-native';
+import { View, TextInput, Text, Pressable } from 'react-native';
+import {useState} from 'react';
 
 import styles from './styles';
+//import react from 'react';
 
 export default function Exemplo4 (){
+    
+    const [txtDigitado, setTxtDigitado] = useState ('Texto digitado');
+    const [txt2, setTxt2] = useState ('');
+    const [txtTela, setTxtTela] = useState ('');
+
+    function atualizaTextoHandle(txt){
+        setTxtDigitado(txt);
+    }
+
     return(
         <View style = {styles.container}>
             <Text style = {styles.titulo}>Exemplo 4</Text>
 
-            <Text style = {styles.texto}>Texto</Text>
+            <Text style = {styles.texto}>{txtDigitado}</Text>
 
-            <TextInput style = {styles.input}/>
+            <TextInput 
+            style = {styles.input}
+            onChangeText={(valor) => atualizaTextoHandle (valor)}
+            />
 
+            <View style = {styles.linha}/>
+
+            <Text style = {styles.texto}>{txtTela}</Text>
+         
+            <TextInput 
+            style = {styles.input}
+            onChangeText={(vlr) => setTxt2 (vlr)}
+            value={txt2}
+            />
+
+            <Pressable 
+            style={({ pressed }) => pressed ? [styles.botao, styles.botaoPress] : styles.botao}>
+                <Text style = {styles.txtBotao}>Exibir texto</Text>
+            </Pressable>
 
         </View>
+
     );
 }
